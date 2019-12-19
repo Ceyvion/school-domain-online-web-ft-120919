b6bfb89a4fe66340726b23b30305735618473fb0
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-19 16:45:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  
+  
+  
+attr_reader :name, :roster
+
+
+def initialize(name)
+  @name = name 
+  @roster = {}
+end
+
+def add_student(name, grade)
+  
+  
+  
+  
+
+  
+end 
+
+
